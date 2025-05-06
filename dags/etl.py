7cb5bff736b796e5992c9cba8910dda5d360f8c3
Yash Gupta <eryash15@gmail.com>
2025-05-07 00:09:38 +0530

    change in my-feature

--- dags/etl.py
+++ dags/etl.py
@@ -89,5 +89,5 @@ with DAG(
     api_response=extract_apod.output
     ## Transform
     transformed_data = transform_apod_data(api_response)
-    ## Load the data in postgres
+    ## Load the data
     load_data_to_postgres(transformed_data)
